89bdc7d893d871324b80bc2a92702d9f83345e2a
hassan elhamaky <hassan.elhamaky@yahoo.com>
2020-02-12 23:47:33 +0200

    footer completed and fixed navbar accomplished

--- js/main.js
+++ js/main.js
@@ -34,4 +34,23 @@ $(document).ready(function(){
             }
         }
     })
+
+    //Sticky navigation menu
+    let nav_offset_top =$('.header-area').height() + 50;
+
+    function navbarFixed(){
+        if($('.header-area').length){
+            $(window).scroll(function(){
+                let scroll = $(window).scrollTop();
+                if(scroll >= nav_offset_top){
+                    $('.header-area .main-menu').addClass('navbar_fixed');
+                }else{
+                    $('.header-area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
 });
